bfc643f0fabf93c3300bf59abdb9d9d8db506ab0
Manpreeth <manpreethjakkuva256@gmail.com>
2025-08-12 22:35:01 +0530

    all setup configured

--- src/app.js
+++ src/app.js
@@ -2,7 +2,6 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 
-
 const app = express()
 
 app.use(cors({
@@ -19,9 +18,12 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+// routes
+import userRouter from './routes/user.routes.js'
 
+// routes declaration
 
-
+app.use('/api/v1/users', userRouter)
 
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ dotenv.config({});
 // import { DB_NAME } from './constants.js'
 
 import connectDB from './db/index.js'
+import { app } from './app.js';
 
 connectDB()
 .then(() => {
