9d1915637055890f1d07c5e9055a20d1aab66752
Manpreeth <manpreethjakkuva256@gmail.com>
2025-08-15 19:51:01 +0530

    add new routes for user.controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,16 @@
 import { Router } from "express";
-import { registerUser,loginUser,logoutUser,refreshAccessToken } from "../controllers/user.controller.js";
+import { 
+    registerUser,
+    loginUser,
+    logoutUser,
+    refreshAccessToken,
+    changePassword,
+    getCurrentUser,
+    updateAccountDetails,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelProfile
+} from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJwt } from "../middlewares/auth.middleware.js";
 
@@ -24,5 +35,17 @@ router.route("/login").post(loginUser)
 router.route("/logout").post(verifyJwt,  logoutUser)
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJwt,changePassword)
+
+router.route("/current-user").get(verifyJwt,getCurrentUser)
+
+router.route("/update-account-details").patch(verifyJwt,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJwt, upload.single("avatar"), updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJwt, upload.single("coverImage"), updateUserCoverImage)
+
+router.route("/c/:userName").get(verifyJwt, getUserChannelProfile)
 
 export default router
